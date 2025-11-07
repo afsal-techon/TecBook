833787b87db5730b501d3506815ba49e91ba10fc
afsal-techon <techon.afsal@gmail.com>
2025-11-07 13:42:02 +0530

    ff

--- server/app.ts
+++ server/app.ts
@@ -17,7 +17,8 @@ const app:Application = express();
 const port  = process.env.PORT;
 
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
 // app.use(cors({
