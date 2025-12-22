0ae0291164de7e53ff7632024cd9b8522bc0c908
muhammed-anees-p <aneespengad4447@gmail.com>
2025-12-22 15:34:30 +0530

    feat(purchase-order): enhance purchase order routes with auth and validation
    - add verifyUser middleware to all purchase order routes
    - implement DTO validation for update purchase order route
    - rename genericFindAll to getAllPurchaseOrders for clarity
    - add getPurchaseOrderById route

--- server/Routes/purchase-order.routes.ts
+++ server/Routes/purchase-order.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import purchaseOrderController from "../controller/PurchaseOrderController/purchase-order.controller";
 import { validateDto } from "../middleware/validate-dto.middleware";
-import { CreatePurchaseOrderDto } from "../dto/create-purchase-order.dto";
+import { CreatePurchaseOrderDto, UpdatePurchaseOrderDto } from "../dto/create-purchase-order.dto";
 import { verifyUser } from "../middleware/auth";
 
 const router = Router();
 
 router.post("/", verifyUser,validateDto(CreatePurchaseOrderDto),purchaseOrderController.createPurchaseOrder);
-router.get("/", purchaseOrderController.genericFindAll);
-router.put("/:id", purchaseOrderController.updatePurchaseOrder);
-router.delete("/:id", purchaseOrderController.genericDeleteOneById);
+router.put("/:id",verifyUser, validateDto(UpdatePurchaseOrderDto), purchaseOrderController.updatePurchaseOrder);
+router.get("/", verifyUser,purchaseOrderController.getAllPurchaseOrders);
+router.get("/:id",verifyUser,  purchaseOrderController.getPurchaseOrderById);
+router.delete("/:id",verifyUser,  purchaseOrderController.genericDeleteOneById);
 
 
 export default router;
