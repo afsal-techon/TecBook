0a2b0b4fdaad5ea4702bf87574a1e4a52ce200aa
muhammed-anees-p <aneespengad4447@gmail.com>
2025-12-22 17:03:36 +0530

    feat(purchase-order): implement permission checks for routes
    - add checkPermission middleware to all purchase order routes
    - enforce role-based access control for creating, updating, reading, and deleting purchase orders

--- server/Routes/purchase-order.routes.ts
+++ server/Routes/purchase-order.routes.ts
@@ -3,14 +3,15 @@ import purchaseOrderController from "../controller/PurchaseOrderController/purch
 import { validateDto } from "../middleware/validate-dto.middleware";
 import { CreatePurchaseOrderDto, UpdatePurchaseOrderDto } from "../dto/create-purchase-order.dto";
 import { verifyUser } from "../middleware/auth";
+import checkPermission from "../middleware/checkPermission";
 
 const router = Router();
 
-router.post("/", verifyUser,validateDto(CreatePurchaseOrderDto),purchaseOrderController.createPurchaseOrder);
-router.put("/:id",verifyUser, validateDto(UpdatePurchaseOrderDto), purchaseOrderController.updatePurchaseOrder);
-router.get("/", verifyUser,purchaseOrderController.getAllPurchaseOrders);
-router.get("/:id",verifyUser,  purchaseOrderController.getPurchaseOrderById);
-router.delete("/:id",verifyUser,  purchaseOrderController.genericDeleteOneById);
+router.post("/", verifyUser,checkPermission('admin','PurchaseOrder','can_create'),validateDto(CreatePurchaseOrderDto),purchaseOrderController.createPurchaseOrder);
+router.put("/:id",verifyUser, checkPermission('admin','PurchaseOrder','can_update'), validateDto(UpdatePurchaseOrderDto), purchaseOrderController.updatePurchaseOrder);
+router.get("/", verifyUser, checkPermission('admin','PurchaseOrder','can_read') ,purchaseOrderController.getAllPurchaseOrders);
+router.get("/:id",verifyUser, checkPermission('admin','PurchaseOrder','can_read') ,purchaseOrderController.getPurchaseOrderById);
+router.delete("/:id",verifyUser,checkPermission('admin','PurchaseOrder','can_delete') , purchaseOrderController.genericDeleteOneById);
 
 
 export default router;
