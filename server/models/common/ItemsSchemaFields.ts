2c96c57b641874fc6650e9798894d1c4356670be
muhammed-anees-p <aneespengad4447@gmail.com>
2025-12-22 10:43:40 +0530

    feat(common): add reusable items schema fields
    - introduce ItemsSchemaFields for consistent item structure across models
    - define common fields such as tax, quantity, rate, amount, and discount
    - improve reusability and maintainability of item-related schemas

---
+++ server/models/common/ItemsSchemaFields.ts
@@ -0,0 +1,17 @@
+import { Schema } from "mongoose";
+
+export const ItemsSchemaFields = {
+  items: [
+    {
+      taxId: { type: Schema.Types.ObjectId, ref: "Tax", default: null },
+      itemId: { type: Schema.Types.ObjectId, ref: "Item", default: null },
+      itemName: { type: String, required: true },
+      qty: { type: Number, default: 1 },
+      tax: { type: Number, default: 0 },
+      rate: { type: Number, default: 0 },
+      amount: { type: Number, default: 0 },
+      unit: { type: String, default: 0 },
+      discount: { type: Number, default: 0 },
+    },
+  ],
+};
