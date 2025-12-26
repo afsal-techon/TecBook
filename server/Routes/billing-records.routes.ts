334a3378bd0545ce66e689dc0c9c604747df2dff
muhammed-anees-p <aneespengad4447@gmail.com>
2025-12-26 11:35:31 +0530

    feat(billing-records): add billing records routes
    - implement new routes for billing records management
    - include authentication and permission checks for secure access
    - integrate DTO validation for incoming data integrity

---
+++ server/Routes/billing-records.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { verifyUser } from "../middleware/auth";
+import checkPermission from "../middleware/checkPermission";
+import { validateDto } from "../middleware/validate-dto.middleware";
+import { CreateBillingRecordsDTO } from "../dto/billing-records.dto";
+import billingRecordsController from "../controller/billingController/billing-records.controller";
+
+const router = Router()
+
+router.post("/", verifyUser,checkPermission('admin','PurchaseOrder','can_create'),validateDto(CreateBillingRecordsDTO),billingRecordsController.createBillingRecords);
+
+
+export default router;
