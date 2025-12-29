8890ed8975439c5ca3f32a5ee21bbeeaa99668f6
muhammed-anees-p <aneespengad4447@gmail.com>
2025-12-29 10:19:56 +0530

    style : format code

--- server/models/ExpenseModel.ts
+++ server/models/ExpenseModel.ts
@@ -3,47 +3,48 @@ import { BaseSchemaFields } from "./common/BaseSchemaFields";
 import { ItemsSchemaFields } from "./common/ItemsSchemaFields";
 import { TaxPreferences } from "../types/enum.types";
 
-const ExpenseModelSchema = new Schema({
-    date:{
-        type:Date,
-        required:true,
-        default:Date.now()
-        
+const ExpenseModelSchema = new Schema(
+  {
+    date: {
+      type: Date,
+      required: true,
+      default: Date.now(),
     },
-    expenseNumber:{
-        type:String,
-        required:true,
-        unique:true
+    expenseNumber: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    customerId:{
-        type:Schema.Types.ObjectId,
-        ref:"Customer",
-        required:true
+    customerId: {
+      type: Schema.Types.ObjectId,
+      ref: "Customer",
+      required: true,
     },
-    branchId:{
-        type:Schema.Types.ObjectId,
-        ref:"Branch",
-        required:true
+    branchId: {
+      type: Schema.Types.ObjectId,
+      ref: "Branch",
+      required: true,
     },
-    taxPreference:{
-        type:String,
-        enum:TaxPreferences,
-        required:true
+    taxPreference: {
+      type: String,
+      enum: TaxPreferences,
+      required: true,
     },
-    paidAccount:{
-        type:Schema.Types.ObjectId,
-        ref:"Account",
-        required:true
+    paidAccount: {
+      type: Schema.Types.ObjectId,
+      ref: "Account",
+      required: true,
     },
-    vendorId:{
-        type:Schema.Types.ObjectId,
-        ref:"Vendor",
-        required:true
+    vendorId: {
+      type: Schema.Types.ObjectId,
+      ref: "Vendor",
+      required: true,
     },
-},
-{
-    timestamps:true
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 ExpenseModelSchema.add(BaseSchemaFields);
 ExpenseModelSchema.add(ItemsSchemaFields);
 
