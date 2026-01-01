1aac405c8d0c24a01ec559d45b20a42f58e0ba55
muhammed-anees-p <aneespengad4447@gmail.com>
2026-01-01 16:19:46 +0530

    feat(payment): add endpoint to get all payment made data
    - implement GET / endpoint for payment made
    - add verifyUser and checkPermission middleware for authorization
    - validate DTO for request

--- server/Routes/paymentesMade.routes.ts
+++ server/Routes/paymentesMade.routes.ts
@@ -9,6 +9,7 @@ const router = Router()
 
 router.post("/", verifyUser,checkPermission('admin','PurchaseOrder','can_create'),validateDto(CreatePaymentMadeDto),paymentMadeController.createPaymentMade);
 router.post("/:id", verifyUser,checkPermission('admin','PurchaseOrder','can_update'),validateDto(UpdatePaymentMadeDto),paymentMadeController.updatePaymentMadeByID);
+router.get("/", verifyUser, checkPermission('admin','PurchaseOrder','can_read') ,paymentMadeController.getAllPaymentMadeData);
 router.get("/:id", verifyUser, checkPermission('admin','PurchaseOrder','can_read') ,paymentMadeController.getSinglePaymentMadeDataById);
 
 
