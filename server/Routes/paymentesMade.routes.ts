76e938b9ca4b5ed6e0636c1af9e8f6129c23ff9e
muhammed-anees-p <aneespengad4447@gmail.com>
2026-01-01 15:18:29 +0530

    feat(payment): add endpoint for updating payment made
    - implement `updatePaymentMadeByID` route to allow modification of existing payment records
    - integrate `UpdatePaymentMadeDto` for request data validation
    - enforce `admin`, `PurchaseOrder`, and `can_update` permissions for access

--- server/Routes/paymentesMade.routes.ts
+++ server/Routes/paymentesMade.routes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { verifyUser } from "../middleware/auth";
 import checkPermission from "../middleware/checkPermission";
 import { validateDto } from "../middleware/validate-dto.middleware";
-import { CreatePaymentMadeDto } from "../dto/paymentMade.dto";
+import { CreatePaymentMadeDto, UpdatePaymentMadeDto } from "../dto/paymentMade.dto";
 import { paymentMadeController } from "../controller/paymentMadeController/paymentMadeController";
 
 const router = Router()
 
 router.post("/", verifyUser,checkPermission('admin','PurchaseOrder','can_create'),validateDto(CreatePaymentMadeDto),paymentMadeController.createPaymentMade);
+router.post("/:id", verifyUser,checkPermission('admin','PurchaseOrder','can_update'),validateDto(UpdatePaymentMadeDto),paymentMadeController.updatePaymentMadeByID);
 
 export default router;
