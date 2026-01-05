b603ba8fb25956a4d86b177848b4cd956f53ee9a
muhammed-anees-p <aneespengad4447@gmail.com>
2026-01-05 12:49:48 +0530

    feat(credit-note): add endpoint to update credit note
    - add `PUT` endpoint `/credit-note/:id` to update existing credit notes
    - implement user verification and admin 'can_update' permission check
    - enable document upload functionality for updates
    - ensure data integrity with `UpdateCreditNoteDto` validation

--- server/Routes/credit-note.routes.ts
+++ server/Routes/credit-note.routes.ts
@@ -3,12 +3,14 @@ import { verifyUser } from "../middleware/auth";
 import checkPermission from "../middleware/checkPermission";
 import { validateDto } from "../middleware/validate-dto.middleware";
 import { upload } from "../middleware/imgUpload";
-import { CreateCreditNoteDto } from "../dto/credit-note.dto";
+import { CreateCreditNoteDto, UpdateCreditNoteDto } from "../dto/credit-note.dto";
 import { creditNoteController } from "../controller/creditNoterController/credit-note.controller";
 
 const router = Router()
 
 router.post("/", verifyUser,checkPermission('admin','CreditNote','can_create'),upload.array('documents',10),validateDto(CreateCreditNoteDto),creditNoteController.createCreditNote);
+router.put("/:id", verifyUser,checkPermission('admin','CreditNote','can_update'),upload.array('documents',10),validateDto(UpdateCreditNoteDto),creditNoteController.updateCreditNoteById);
+
 
 
 
