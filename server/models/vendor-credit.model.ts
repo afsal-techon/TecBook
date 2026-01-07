0991a7fb12e0184051027795e22ce48f767003ec
muhammed-anees-p <aneespengad4447@gmail.com>
2026-01-07 11:42:21 +0530

    feat(model): add vendor credit model
    - create new mongoose schema for vendor credits
    - define fields for vendor identification, credit details, dates, and financial summaries
    - integrate common item and base schema fields for consistency

---
+++ server/models/vendor-credit.model.ts
@@ -0,0 +1,97 @@
+import { model, Schema } from "mongoose";
+import { IVendorCredit } from "../Interfaces/vendor-credit.interface";
+import { ItemsSchemaFields } from "./common/ItemsSchemaFields";
+import { BaseSchemaFields } from "./common/BaseSchemaFields";
+import { commonStatus, PurchaseOrderDiscountType } from "../types/enum.types";
+
+export const vendorCreditSchema = new Schema<IVendorCredit>(
+  {
+    vendorId: {
+      type: Schema.Types.ObjectId,
+      ref: "vendors",
+    },
+    vendorCreditNoteNumber: {
+      type: String,
+      required: true,
+    },
+    referenceNumber: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    branchId: {
+      type: Schema.Types.ObjectId,
+      ref: "Branch",
+      required: true,
+    },
+    note: {
+      type: String,
+      default: null,
+    },
+    terms: {
+      type: String,
+      default: null,
+    },
+    discountType: {
+      type: String,
+      enum: PurchaseOrderDiscountType,
+      default: PurchaseOrderDiscountType.PERCENTAGE,
+    },
+    discountValue: {
+      type: Number,
+      default: 0,
+    },
+    vatValue: {
+      type: Number,
+      default: 0,
+    },
+    status: {
+      type: String,
+      enum: commonStatus,
+    },
+    documents: {
+      type: [String],
+      default: [],
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    taxTotal: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    total: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    balanceAmount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+vendorCreditSchema.add(ItemsSchemaFields);
+vendorCreditSchema.add(BaseSchemaFields);
+export const vendorCreditModel = model<IVendorCredit>(
+  "vendorCredit",
+  vendorCreditSchema
+);
+export type vendorModelDocumentType = typeof vendorCreditModel;
