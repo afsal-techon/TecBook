4b76da98330513eb330df6a340a7056dd9251d06
muhammed-anees-p <aneespengad4447@gmail.com>
2026-01-09 15:55:05 +0530

    feat(account): add account type interface
    - define IAccountType interface for consistent account type structure
    - include properties such as name, code, category, and description
    - extend IBaseFields for standard fields

---
+++ server/Interfaces/account-type.interface.ts
@@ -0,0 +1,10 @@
+import { accountTypesCategory } from "../types/enum.types";
+import { IBaseFIelds } from "./base.interface";
+
+export interface IAccountType extends IBaseFIelds {
+  name: string;
+  code?: string;
+  category: accountTypesCategory;
+  description?: string;
+  isDeletable?: boolean;
+}
